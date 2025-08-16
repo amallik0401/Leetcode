f5600d6d6f8ae3fa7fd9773546cf67e1b15eb253
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-08-16 22:48:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (83.75%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) 
+    {
+        if(root==null)
+        return true;
+
+        return helper(root.left , root.right);
+    }
+
+    boolean helper(TreeNode ln , TreeNode rn)
+    {
+        if(ln==null && rn==null)
+        {
+            return true;
+        }
+        
+        if(ln==null || rn==null)
+        {
+            return false;
+        }
+
+        if(ln.val!=rn.val)
+        {
+            return false;
+        }
+
+        return helper(ln.left , rn.right) && helper(rn.left , ln.right);
+    }
+}
