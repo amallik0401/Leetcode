48fc0403ae7e90cc872eb8b4f05357ca880a62e2
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-08-31 02:50:05 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (22.37%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,52 +1,57 @@
-class Solution {
-    public int myAtoi(String s) {
-        if (s == null || s.length() == 0) {
+class Solution 
+{
+    public int myAtoi(String s) 
+    {
+        if(s.length() == 0 || s==null )
+        {
             return 0;
         }
-        
-        // Constants for 32-bit signed integer range
-        final int INT_MAX = Integer.MAX_VALUE;
-        final int INT_MIN = Integer.MIN_VALUE;
-        
-        int i = 0;
-        int n = s.length();
-        
-        // Step 1: Skip leading whitespace
-        while (i < n && s.charAt(i) == ' ') {
-            i++;
+        final int max = Integer.MAX_VALUE;
+        final int min = Integer.MIN_VALUE;
+
+        int b = 0 ;
+        int c = 1;
+
+        while(b<s.length() &&  s.charAt(b)==' ')
+        {
+            b++;
         }
-        
-        // Check if we've reached the end
-        if (i == n) {
+        if (b == s.length()) 
+        {
             return 0;
         }
-        
-        // Step 2: Check for sign
-        int sign = 1;
-        if (s.charAt(i) == '+') {
-            i++;
-        } else if (s.charAt(i) == '-') {
-            sign = -1;
-            i++;
+
+        if(s.charAt(b) == '-')
+        {
+            c = -1;
+            b++;
         }
-        
-        // Step 3: Read digits and convert
-        long res = 0;
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i) - '0';
-            res = res * 10 + digit;
-            
-            if (sign * res <= INT_MIN) {
-                return INT_MIN;
-            }
-            if (sign * res >= INT_MAX) {
-                return INT_MAX;
-            }
-            
-            i++;
+        else if(s.charAt(b) == '+')
+        {
+            b++;
         }
+
+        long a = 0;
+        while(b<s.length() && Character.isDigit(s.charAt(b)))
+        {
+            int digit = s.charAt(b) - '0';
+
+            a = a*10 + digit;
+
+
+            if (c * a <= min)
+            {
+                return min;
+            }
+
+            if (c * a >= max) 
+            {
+                return max;
+            }
+            b++;
+        }
+
+        return (int)(c*a);
         
-        // Step 4: Apply sign and return
-        return (int)(res * sign);        
     }
 }
