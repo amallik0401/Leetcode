db842be2c5373c9f75b7949b957938ad676a276c
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-09-05 01:40:09 +0530

    Time: 2 ms (55.24%) | Memory: 46.2 MB (60.43%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,16 +1,18 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
-        int[] new_arr = new int[(nums1.length) + (nums2.length)];
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+        int[] new_arr = new int[n];
 
         int i=0, j=0, k=0;
 
-        while (i<=nums1.length && j<=nums2.length) {
-            if (i == nums1.length) {
-                while(j<nums2.length) new_arr[k++] = nums2[j++];
+        while (i<=n1 && j<=n2) {
+            if (i == n1) {
+                while(j<n2) new_arr[k++] = nums2[j++];
                 break;
-            } else if (j == nums2.length) {
-                while (i<nums1.length) new_arr[k++] = nums1[i++];
+            } else if (j == n2) {
+                while (i<n1) new_arr[k++] = nums1[i++];
                 break;
             }
 
@@ -21,7 +23,7 @@ class Solution {
             }
         }
 
-        if (((nums1.length) + (nums2.length))%2==0) return (float)(new_arr[((nums1.length) + (nums2.length))/2-1] + new_arr[((nums1.length) + (nums2.length))/2])/2;
-        else return new_arr[((nums1.length) + (nums2.length))/2];
+        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
+        else return new_arr[n/2];
     }
 }
