5665a23480011caa7e67076c2afbcfe89a750c30
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-09-05 01:36:08 +0530

    Time: 2 ms (55.24%) | Memory: 46.3 MB (26.77%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,30 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+        int[] new_arr = new int[n];
 
-        ArrayList<Integer> list = new ArrayList<>();
-        double ans = 0;
+        int i=0, j=0, k=0;
 
-        for(int num: nums1)
-        {
-            list.add(num);
+        while (i<=n1 && j<=n2) {
+            if (i == n1) {
+                while(j<n2) new_arr[k++] = nums2[j++];
+                break;
+            } else if (j == n2) {
+                while (i<n1) new_arr[k++] = nums1[i++];
+                break;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                new_arr[k++] = nums1[i++];
+            } else {
+                new_arr[k++] = nums2[j++];
+            }
         }
-        for(int num : nums2)
-        {
-            list.add(num);
-        }
 
-        Collections.sort(list);
-        int count = list.size();
-
-        if(count%2!=0)
-            ans = (double) list.get((count-1)/2);
-        else
-        {
-            int first = count / 2;
-            int second = (count - 1) / 2;
-            ans = (double) (list.get(first) + list.get(second)) / 2;
-        }
-
-        return ans;
+        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
+        else return new_arr[n/2];
     }
 }
