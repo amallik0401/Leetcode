fb358cd1de9694c8e5e35dd0b628e4f2c7f01079
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-10-07 00:05:36 +0530

    Time: 8 ms (70.49%) | Memory: 60.5 MB (38.09%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,27 +1,34 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return 0;
-        }
-        
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[][] dp = new int[rows][cols];
-        int maxSide = 0;
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == '1') {
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
-                    }
-                    maxSide = Math.max(maxSide, dp[i][j]);
+       
+       if(matrix.length==0 || matrix[0].length==0)
+       {
+        return 0;
+       }
+
+       int row = matrix.length;
+       int col = matrix[0].length;
+       int [][]arr = new int[row][col];
+       int max = 0;
+
+       for(int i = 0 ; i< row ; i++)
+       {
+        for(int j = 0 ; j<col ; j++)
+        {
+            if(matrix[i][j]=='1')
+            {
+                if(i==0 || j==0)
+                {
+                    arr[i][j] =1;
                 }
+                else
+                {
+                    arr[i][j] = Math.min(Math.min(arr[i-1][j],arr[i][j-1]),arr[i-1][j-1])+1;
+                }
+                max = Math.max(max , arr[i][j]);
             }
         }
-        
-        return maxSide * maxSide;
+       }
+       return max*max;
     }
 }
