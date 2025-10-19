596740450a1c43f9b9e05e604308bc12793e8ca7
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-10-19 23:08:36 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (75.73%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,46 +1,51 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int index = -1;
-        int n = nums.length;
-
-        // Find the first decreasing element from end
-        for(int i = n-2; i>=0; i--) {
-            if(nums[i] < nums[i + 1]) {
-                index = i;
-                break;
-            }
-        }
-
-        //if no index found
-        if(index == -1) {
-            reverse(nums, 0, n-1);
-            return;
-        }
-
-        //if index found
-        for(int i = n-1; i > index; i--) {
-            if(nums[i] > nums[index]) {
-                swap(nums, i, index);
-                break;
-            }
-        }
-        
-        //reverse part after index
-        reverse(nums, index+1, n-1);
-
-    }
-
-    private void reverse(int arr[], int start, int end) {
-        while(start < end) {
-            swap(arr, start, end);
-            start++;
-            end--;
+    
+    int ind = -1;
+    int n = nums.length;
+    for(int i= n-2 ; i>=0 ; i--)
+    {
+        if(nums[i]<nums[i+1])
+        {
+            ind = i;
+            break;
         }
     }
 
-    private void swap(int arr[], int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    if(ind==-1)
+    {
+        rev(nums,0,n-1);
+        return;
+    }
+
+    for(int i = n-1 ; i>ind ; i--)
+    {
+        if(nums[i]>nums[ind])
+        {
+            swap(nums , i , ind);
+            break;
+        }
+    }
+    
+
+    rev(nums, ind+1 ,n-1);
+
+    }
+
+    private void swap(int []a , int i , int j)
+    {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private void rev(int []aa , int i , int j)
+    {
+        while(i<j)
+        {
+            swap(aa , i , j);
+            i++;
+            j--;
+        }
     }
 }
