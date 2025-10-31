707873c34eb916cdb4939a6880adc0b788f7f6ef
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-10-31 22:14:59 +0530

    Time: 2 ms (63.09%) | Memory: 46.8 MB (7.71%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -3,24 +3,16 @@ class Solution {
         Set<Integer> hs = new HashSet<>();
         int []ans = new int[2];
         int p = 0;
-
         for(int i = 0 ; i <nums.length ; i++)
         {
-            if(p==2)
-                break;
-
             if(hs.contains(nums[i]))
             {
                 ans[p] = nums[i];
                 p++;
             }
             else
-            {
                 hs.add(nums[i]);
-            }
         }
-
         return ans;
-
     }
 }
