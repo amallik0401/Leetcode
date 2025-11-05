72e6db11079271fbfca67a1e5bba56fe944f5fd3
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-11-06 00:01:00 +0530

    Time: 3 ms (51.35%) | Memory: 43.8 MB (9.11%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,104 +1,24 @@
 class Solution {
-    // Function to add two strings.
-    private ArrayList<Integer> addStrings(
-        ArrayList<Integer> num1,
-        ArrayList<Integer> num2
-    ) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        int carry = 0;
-
-        for (int i = 0; i < num1.size() || i < num2.size(); ++i) {
-            // If num2 is shorter than num1 or vice versa, use 0 as the current digit.
-            int digit1 = i < num1.size() ? num1.get(i) : 0;
-            int digit2 = i < num2.size() ? num2.get(i) : 0;
-
-            // Add current digits of both numbers.
-            int sum = digit1 + digit2 + carry;
-            // Set carry equal to the tens place digit of sum.
-            carry = sum / 10;
-            // Append the ones place digit of sum to answer.
-            ans.add(sum % 10);
-        }
-
-        if (carry != 0) {
-            ans.add(carry);
-        }
-        return ans;
-    }
-
-    // Multiply the current digit of secondNumber with firstNumber.
-    ArrayList<Integer> multiplyOneDigit(
-        StringBuilder firstNumber,
-        char secondNumberDigit,
-        int numZeros
-    ) {
-        // Insert zeros at the beginning based on the current digit's place.
-        ArrayList<Integer> currentResult = new ArrayList<>();
-        for (int i = 0; i < numZeros; ++i) {
-            currentResult.add(0);
-        }
-
-        int carry = 0;
-
-        // Multiply firstNumber with the current digit of secondNumber.
-        for (int i = 0; i < firstNumber.length(); ++i) {
-            char firstNumberDigit = firstNumber.charAt(i);
-            int multiplication =
-                (secondNumberDigit - '0') * (firstNumberDigit - '0') + carry;
-            // Set carry equal to the tens place digit of multiplication.
-            carry = multiplication / 10;
-            // Append last digit to the current result.
-            currentResult.add(multiplication % 10);
-        }
-
-        if (carry != 0) {
-            currentResult.add(carry);
-        }
-        return currentResult;
-    }
-
     public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) {
-            return "0";
+        if (num1.equals("0") || num2.equals("0")) return "0";
+
+        int n1 = num1.length();
+        int n2 = num2.length();
+        int[] result = new int[n1 + n2];
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int sum = mul + result[i + j + 1];
+
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+    }}
+    StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
         }
 
-        StringBuilder firstNumber = new StringBuilder(num1);
-        StringBuilder secondNumber = new StringBuilder(num2);
-
-        // Reverse both the numbers.
-        firstNumber.reverse();
-        secondNumber.reverse();
-
-        // To store the multiplication result of each digit of secondNumber with firstNumber.
-        int N = firstNumber.length() + secondNumber.length();
-        ArrayList<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < N; ++i) {
-            ans.add(0);
-        }
-
-        // For each digit in secondNumber, multipy the digit by firstNumber and
-        // add the multiplication result to ans.
-        for (int i = 0; i < secondNumber.length(); ++i) {
-            // Add the current result to final ans.
-            ans = addStrings(
-                multiplyOneDigit(firstNumber, secondNumber.charAt(i), i),
-                ans
-            );
-        }
-
-        // Pop excess 0 from the rear of ans.
-        if (ans.get(ans.size() - 1) == 0) {
-            ans.remove(ans.size() - 1);
-        }
-
-        // Ans is in the reversed order.
-        // Copy it in reverse order to get the final ans.
-        StringBuilder answer = new StringBuilder();
-
-        for (int i = ans.size() - 1; i >= 0; --i) {
-            answer.append(ans.get(i));
-        }
-
-        return answer.toString();
-    }
+        return sb.length() == 0 ? "0" : sb.toString();}
 }
