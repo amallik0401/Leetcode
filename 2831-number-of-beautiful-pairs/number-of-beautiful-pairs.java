81b891556daa25508749581466e5d6259bcfa663
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-12-02 12:57:39 +0530

    Time: 18 ms (57.45%) | Memory: 46.3 MB (50.71%) - LeetSync

---
+++ 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int countBeautifulPairs(int[] nums) {
+        
+        int n = nums.length;
+        int count=0;
+        for(int i = 0 ; i<n-1; i++)
+        {
+            for(int j =i+1 ; j<n ; j++)
+            {
+                int a = nums[i];
+                while(a>=10)
+                {
+                    a/=10;
+                    
+                }
+                int b = nums[j]%10;
+
+                if(gcd(a,b))
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+        
+    }
+
+    private boolean gcd(int x,int y)
+    {
+        while(y!=0)
+        {
+            int temp = y;
+            y = x%y;
+            x = temp;
+        }
+
+        return x==1;
+    }
+}
