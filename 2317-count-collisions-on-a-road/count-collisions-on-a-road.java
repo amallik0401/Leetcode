540951485c97188b13bc61fe192d3010ca008b76
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-12-05 23:29:51 +0530

    Time: 1256 ms (5.22%) | Memory: 49.1 MB (7.63%) - LeetSync

---
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -0,0 +1,81 @@
+class Solution {
+    public int countCollisions(String directions) {
+
+        Stack<Character> st = new Stack<>();
+
+        int count = 0;
+        char[] ch = directions.toCharArray();
+        for(char s : ch)
+        {
+            if(st.isEmpty())
+            {
+                st.push(s); 
+                continue;
+            }
+            char cr = st.peek();
+
+            if(s == 'R' && cr == s)
+            {
+                st.push('R');
+                continue;        
+            }
+            if(cr != s)
+            {
+                if(cr == 'S')
+                {
+                    if(s == 'L')
+                        count++;
+                    else
+                    {
+                        st.push('R');
+                    }
+                }
+                else if(s == 'S')
+                {
+                    if(cr == 'R')
+                    {
+                        st.pop();
+                        count++;
+                    }
+                    
+                    st.push('S');
+                }
+                else if(cr == 'R' && s== 'L')
+                {
+                    count+=2;
+                    st.pop();
+                    st.push('S');
+                }
+                else
+                {
+                    st.pop();
+                    st.push(s);
+                }
+            }
+        }  
+
+        for(char c : st)
+        {
+            System.out.println(c);
+        }
+
+        char prev = 'a';
+        while(!st.isEmpty())
+        {
+            char top = st.pop();
+
+            if(top == prev)
+            {
+                continue;
+            }
+
+            if(top == 'R' && prev == 'S')
+            {
+                count++;
+                st.push('S');
+            }
+            prev = top;
+        }
+        return count;      
+    }
+}
