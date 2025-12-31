715a9c97f7228e69961f33829f046508c51da2d6
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2025-12-31 12:29:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (35.36%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.java
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.java
@@ -3,15 +3,10 @@ class Solution {
 
         int []nums = new int[n];
         nums[0] = start;
+        int res = nums[0];
         for(int i = 1 ; i < n ; i++)
         {
             nums[i] = start + 2*i;
-        }
-
-        int res = nums[0];
-
-        for(int i = 1 ; i < n ;i++)
-        {
             res^=nums[i];
         }
         return res;
