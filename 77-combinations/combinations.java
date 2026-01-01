139e26e318d4cc92640803601df25fe344b4aaca
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-01-02 02:30:38 +0530

    Time: 18 ms (80.65%) | Memory: 98.3 MB (14.05%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,15 +1,16 @@
 class Solution {
-    public List<List<Integer>> combine(int n, int k) {
-
     List<List<Integer>> ans = new ArrayList<>();
+    int k ;
+    public List<List<Integer>> combine(int n, int k) {
+        this.k = k;
     List<Integer> curr = new ArrayList<>();
 
-    BT(1 , curr , ans , n , k);
+    BT(1 , curr ,n );
 
     return ans;
     }
 
-    void BT(int x , List<Integer> curr , List<List<Integer>> ans , int n , int k)
+    void BT(int x , List<Integer> curr , int n )
     {
         if(curr.size() == k)
         {
@@ -20,7 +21,7 @@ class Solution {
         for(int i = x ; i <= n ;i++)
         {
             curr.add(i);
-            BT(i+1,curr , ans , n , k);
+            BT(i+1,curr , n );
             curr.remove(curr.size()-1);
         }
 
