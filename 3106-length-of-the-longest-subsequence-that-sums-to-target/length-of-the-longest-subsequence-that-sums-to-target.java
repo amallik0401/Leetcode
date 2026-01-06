b9c23808662acf2e591bf63f2eed3b5a7267365d
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-01-07 01:02:15 +0530

    Time: 259 ms (46.47%) | Memory: 60.2 MB (22.65%) - LeetSync

---
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.java
@@ -0,0 +1,27 @@
+class Solution
+{
+    public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
+    int n = nums.size();
+
+    int[][] dp = new int[n+1][target + 1];
+    for (int[] row : dp) {
+        Arrays.fill(row, -1);
+    }
+
+    for (int i = 0; i<=n; i++) {
+        dp[i][0] = 0;
+    }
+
+    for (int i = 1; i<n+1; i++) {
+        for (int j = 1; j < target+ 1; j++) {
+            // case 1: if we don't take nums[i-1]
+            dp[i][j] = dp[i-1][j];
+            // case 2: if we take take nums[i-1]
+            if (j >= nums.get(i-1) && dp[i-1][j - nums.get(i-1)] != -1) {
+                dp[i][j] = Math.max(dp[i][j], 1 + dp[i-1][j - nums.get(i-1)]);
+            }
+        }
+    }
+    return dp[n][target];
+}
+}
