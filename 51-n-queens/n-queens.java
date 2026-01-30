8b0ff0e3485d9d8accd086edb68a4ded753d792a
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-01-31 01:52:45 +0530

    Time: 2 ms (79.90%) | Memory: 46.7 MB (57.80%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,68 @@
+class Solution {
+    List<List<String>> ans = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) 
+    {
+
+        char[][] board = new char[n][n];
+
+        for(char[] row : board)
+        {
+            Arrays.fill(row , '.');
+        }
+
+        helper(board , 0);
+        return ans;
+    }
+
+    void helper(char[][] board , int row)
+    {
+        if(row == board.length)
+        {
+            List<String> list = new ArrayList<>();
+
+            for(int i = 0 ; i < board.length ; i++)
+            {
+                list.add(new String(board[i]));
+            }
+            ans.add(list);
+            return;
+        }
+
+        for(int col = 0 ; col < board[row].length ; col++)
+        {
+            if(isSafe(board , row , col))
+            {
+                board[row][col] = 'Q';
+                helper(board , row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    public boolean isSafe(char[][] board , int row , int col)
+    {
+        for(int i = 0 ; i < row ; i++)
+        {
+            if(board[i][col] == 'Q')
+                return false;
+        }
+
+        int maxLeft = Math.min(row , col);
+
+        for(int i = 1 ; i <= maxLeft ; i++)
+        {
+            if(board[row-i][col-i] == 'Q')
+                return false;
+        }
+
+        int maxRight = Math.min(row , board.length-1-col);
+
+        for(int i = 1 ; i <= maxRight ; i++)
+        {
+            if(board[row - i][col + i] == 'Q')
+                return false;
+        }
+        return true;
+    }
+}
