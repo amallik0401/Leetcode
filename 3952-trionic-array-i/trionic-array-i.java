fa6abf9de992b6c7792fc05e340d5b60cf9fdd81
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-02-04 19:02:31 +0530

    Time: 1 ms (71.97%) | Memory: 44.7 MB (65.69%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -2,30 +2,35 @@ class Solution {
     public boolean isTrionic(int[] nums) {
         
         int n = nums.length;
-        int peak = -1 , valley = n;
+        int i = 0;
 
-        for(int i = 0 ; i < n-1 ;i++)
+        //increasing
+        while(i + 1 < n && nums[i] < nums[i+1])
         {
-            if(peak == -1 && nums[i] >=nums[i+1])
-                peak = i;
-
-            if(valley == n && nums[n-1-i] <= nums[n-2-i])
-                valley = n - 1 - i;
-            
-            if(peak > 0 && valley < n-1 && peak < valley)
-                return isDecreasing(nums , peak , valley);
+            i++;
         }
 
-        return false;
-    }
-
-    boolean isDecreasing(int[] nums , int a , int b)
-    {
-        for(int i = a ; i < b ; i++)
+        if(i == 0 || i == n-1)
         {
-            if(nums[i] <= nums[i+1])
-                return false;
+            return false;
         }
-        return true;
+
+        //decreasing
+        while(i + 1 < n && nums[i] > nums[i+1])
+        {
+            i++;
+        }
+
+        if(i == n-1)
+        {
+            return false;
+        }
+
+        while(i + 1 < n && nums[i] < nums[i+1])
+        {
+            i++;
+        }
+
+        return i == n -1;
     }
 }
