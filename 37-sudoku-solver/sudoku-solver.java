2be9eaa4ba3cbe31e4ffa46e2a04800d1f19de65
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-02-05 23:13:03 +0530

    Time: 387 ms (9.44%) | Memory: 43 MB (27.68%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,56 +1,46 @@
 class Solution {
-    int n = 3;
-    int N = n * n;
-    int[][] rows = new int[N][N + 1];
-    int[][] columns = new int[N][N + 1];
-    int[][] boxes = new int[N][N + 1];
-    char[][] board;
-    boolean sudokuSolved = false;
-
-    public boolean couldPlace(int d, int row, int col) {
-        int idx = (row / n) * n + col / n;
-        return rows[row][d] + columns[col][d] + boxes[idx][d] == 0;
-    }
-
-    public void placeNumber(int d, int row, int col) {
-        int idx = (row / n) * n + col / n;
-        rows[row][d]++;
-        columns[col][d]++;
-        boxes[idx][d]++;
-        board[row][col] = (char)(d + '0');
-    }
-
-    public void removeNumber(int d, int row, int col) {
-        int idx = (row / n) * n + col / n;
-        rows[row][d]--;
-        columns[col][d]--;
-        boxes[idx][d]--;
-        board[row][col] = '.';
-    }
-
-    public void placeNextNumbers(int row, int col) {
-        if (row == N - 1 && col == N - 1) sudokuSolved = true;
-        else if (col == N - 1) backtrack(row + 1, 0);
-        else backtrack(row, col + 1);
-    }
-
-    public void backtrack(int row, int col) {
-        if (board[row][col] == '.') {
-            for (int d = 1; d <= 9; d++) {
-                if (couldPlace(d, row, col)) {
-                    placeNumber(d, row, col);
-                    placeNextNumbers(row, col);
-                    if (!sudokuSolved) removeNumber(d, row, col);
-                }
-            }
-        } else placeNextNumbers(row, col);
-    }
 
     public void solveSudoku(char[][] board) {
-        this.board = board;
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                if (board[i][j] != '.') placeNumber(Character.getNumericValue(board[i][j]), i, j);
-        backtrack(0, 0);
+        
+        solve(board);
+         
+    }
+
+    boolean solve(char[][]board)
+    {
+        for(int i = 0 ; i < board.length ; i++)
+        {
+            for(int j = 0 ; j < board[0].length ; j++)
+            {
+                if(board[i][j] == '.')
+                {
+                    for(char c = '1' ; c <= '9' ; c++)
+                    {
+                        if(isValid(board , i , j , c))
+                        {
+                            board[i][j] = c;
+
+                            if(solve(board))
+                                return true;
+
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean isValid(char [][]board , int row , int col , char c)
+    {
+        for(int i = 0 ; i < 9 ; i++)
+        {
+            if(board[row][i] == c) return false;
+            if(board[i][col] == c) return false;
+            if(board[3 * (row / 3) + i / 3] [3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
     }
 }
