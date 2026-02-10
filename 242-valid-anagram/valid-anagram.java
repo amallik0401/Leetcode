942de3379baa0886fc64ff8bae527411667dab5e
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-02-10 22:06:03 +0530

    Time: 3 ms (90.94%) | Memory: 46.7 MB (23.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
 
         char []arr1 = s.toCharArray();
         char []arr2 = t.toCharArray();
@@ -7,7 +8,6 @@ class Solution {
         Arrays.sort(arr1);
         Arrays.sort(arr2);
 
-        if(arr1.length!=arr2.length) return false;
 
         for(int i = 0; i < arr1.length ; i++)
         {
@@ -15,6 +15,5 @@ class Solution {
         }
 
         return true;
-        
     }
 }
