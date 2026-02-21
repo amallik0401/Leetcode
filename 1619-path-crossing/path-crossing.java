689506208141f43badb9998d22cd878623718122
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-02-22 00:32:00 +0530

    Time: 10 ms (5.88%) | Memory: 43.9 MB (7.50%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        Set<String>set = new HashSet<>();
+        set.add("0,0");
+        int x = 0;
+        int y = 0;
+
+        for(char c : path.toCharArray())
+        {
+            if(c == 'N')
+            {
+                y++;
+            }
+            else if(c == 'E')
+            {
+                x++;
+            }
+            else if(c == 'S')
+            {
+                y--;
+            }
+            else
+            {
+                x--;
+            }
+
+            String s = x+"," + y;
+            System.out.print(s);
+
+            if(set.contains(s))
+            {
+                return true;
+            }
+            else
+            {
+                set.add(s);
+            }
+        }
+
+        return false;
+
+    }
+}
