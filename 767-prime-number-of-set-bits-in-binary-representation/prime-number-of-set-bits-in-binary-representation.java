a653caa6794b0e1f499f5e0a66d76395873a141b
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-02-21 14:48:00 +0530

    Time: 28 ms (26.62%) | Memory: 42.3 MB (79.37%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -2,24 +2,24 @@ class Solution {
     List<Integer> prime=new ArrayList<>(Arrays.asList(2,3,5,7,11,13,17,19,23,29,31));
     
 
-    public int binary(int num) {
-        int count=0;
-        // String s="";
-        while(num!=0) {
-            int r=num%2;
-            if(r==1) count++;
-            // s=r+s;
-            num=num/2;
-        }
-        return count;
-    }
+    // public int binary(int num) {
+    //     int count=0;
+    //     // String s="";
+    //     while(num!=0) {
+    //         int r=num%2;
+    //         if(r==1) count++;
+    //         // s=r+s;
+    //         num=num/2;
+    //     }
+    //     return count;
+    // }
     
     public int countPrimeSetBits(int left, int right) {
         int ans=0;
 
         while(left<=right) {
-            int count=binary(left);
-            if(prime.contains(count)) ans++;
+            int bits = Integer.bitCount(left);
+            if(prime.contains(bits)) ans++;
             left++;
         }
 
