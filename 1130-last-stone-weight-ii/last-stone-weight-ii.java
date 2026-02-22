c243d7b887f0a2d3e22e612ce8940f8b93d6d9c8
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-02-22 11:50:31 +0530

    Time: 5 ms (33.90%) | Memory: 43.2 MB (55.88%) - LeetSync

---
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        int n = stones.length;
+        int total = 0;
+
+        for(int i : stones)
+        {
+            total += i;
+        }
+
+        boolean [][]dp = new boolean[n+1][total+1];
+
+        for(int i = 0 ; i <= n ; i++)
+        {
+            dp[i][0] = true;
+        }
+
+        for(int i = 1 ; i <= n ; i++)
+        {
+            for(int j = 1 ; j <= total ; j++)
+            {
+                if(j<stones[i-1])
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j- stones[i-1]];
+                }
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0 ; i <= total ; i++)
+        {
+            if(dp[n][i])
+                min = Math.min(min , Math.abs((total - i) - i));
+        }
+
+        return min;
+    }
+}
