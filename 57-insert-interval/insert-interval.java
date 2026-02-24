f77d56e32aba5715604b25e41816cecb43f0ae74
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-02-25 01:36:02 +0530

    Time: 2 ms (28.61%) | Memory: 47.3 MB (17.28%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,30 +1,37 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] nI) {
 
-        List<int[]> merge = new ArrayList<>(Arrays.asList(intervals));
-        merge.add(nI);
-        Collections.sort(merge , (a,b) -> Integer.compare(a[0],b[0]));
-
-        List<int []> res = new ArrayList<>();
-        int [] prev = merge.get(0);
-
-        for(int i = 1 ; i < merge.size() ; i++)
+        int n = intervals.length;
+        int[][] merged = new int[n+1][];
+        for(int i = 0 ; i < n ; i++)
         {
-            int[]interval = merge.get(i);
+            merged[i] = intervals[i];
+        }
+        merged[n] = nI;
+
+        Arrays.sort(merged , (a,b) -> Integer.compare(a[0],b[0]));
+
+        
+        List<int[]> merge = new ArrayList<>();
+        int [] prev = merged[0];
+
+        for(int i = 1 ; i<= n ; i++)
+        {
+            int[]interval = merged[i];
             if(interval[0] <= prev[1])
             {
                 prev[1] = Math.max(prev[1] , interval[1]);
             }
             else
             {
-                res.add(prev);
+                merge.add(prev);
                 prev = interval;
             }
         }
 
-        res.add(prev);
+        merge.add(prev);
 
-        return res.toArray(new int[res.size()][]);
+        return merge.toArray(new int[merge.size()][]);
 
     }
 }
