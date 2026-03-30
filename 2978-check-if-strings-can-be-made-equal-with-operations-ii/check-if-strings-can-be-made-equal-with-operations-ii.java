3d40e6683d73e8171d17931bfdefc4bf1d259434
Amit Mallik <166247642+amallik0401@users.noreply.github.com>
2026-03-30 11:00:44 +0530

    Time: 5 ms (98.68%) | Memory: 47.9 MB (85.53%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,76 +1,16 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        Map<Character , Integer> even = new HashMap<>();
-        Map<Character , Integer> odd = new HashMap<>();
 
-        for(int i =0 ; i < s1.length() ; i++)
-        {
-            char ch = s1.charAt(i);
-
-            if(i%2==0)
-            {
-                if(even.containsKey(ch))
-                {
-                    even.put(ch , even.get(ch) + 1);
-                }
-                else
-                {
-                    even.put(ch , 1);
-                }
-            }
-            else
-            {
-                if(odd.containsKey(ch))
-                {
-                    odd.put(ch , odd.get(ch) + 1);
-                }
-                else
-                {
-                    odd.put(ch , 1);
-                }
-            }
-        }
-
-        for(int i =0 ; i < s1.length() ; i++)
-        {
-            char ch = s2.charAt(i);
-
-            if(i%2==0)
-            {
-                if(even.containsKey(ch))
-                {
-                    even.put(ch , even.get(ch) - 1);
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                if(odd.containsKey(ch))
-                {
-                    odd.put(ch , odd.get(ch) - 1);
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-
-        for (Character key : even.keySet()) 
-        {
-            if(even.get(key) != 0)
-                return false;
-        }
-        for (Character key : odd.keySet()) 
-        {
-            if(odd.get(key) != 0)
-                return false;
-        }
-
-
+        int[][] freq = new int[2][26];
+		for (int i = 0; i < s1.length(); i++) {
+			freq[i % 2][s1.charAt(i) - 'a']++;
+			freq[i % 2][s2.charAt(i) - 'a']--;
+		}
+		for (int i = 0; i < 26; i++) {
+			if (freq[0][i] != 0 || freq[1][i] != 0) {
+				return false;
+			}
+		}
         return true;
     }
 }
